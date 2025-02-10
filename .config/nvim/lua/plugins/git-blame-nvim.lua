856a4d22759093243b2a7b4c0c30158a79dcb462
dbertak <dbertak@gmail.com>
2025-02-10 23:55:06 +0100

    inline git plugin added

--- .config/nvim/lua/plugins/git-blame-nvim.lua
+++ .config/nvim/lua/plugins/git-blame-nvim.lua
@@ -4,8 +4,10 @@ return {
   'f-person/git-blame.nvim',
   event = 'VeryLazy',
   opts = {
-    enabled = false, -- disable by default, enabled only on keymap
-    date_format = '%m/%d/%y %H:%M:%S', -- more concise date format
-  }
+    enabled = true,  -- if you want to enable the plugin
+    date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
+    virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+  },
 }
+
 
